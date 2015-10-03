d394adc6f2688ccc39367b894ec146ba4cf44708
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2015-10-03 23:52:43 +0200

    Init

---
+++ app/src/main/kotlin/com/kamosoft/nirvana/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kamosoft.nirvana;
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import nirvana.kamosoft.com.nirvana.R
+
+public class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_main)
+    }
+}
